27821362611261009589ff5dd0fd2a4027b50f09
Cooper Brislain <cbrislain@gmail.com>
2019-09-16 14:08:52 -0700

    added firebase

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,20 @@
+/* FIREBASE */
+var firebaseConfig = {
+    apiKey: "AIzaSyACSShSnvrgEte-P-TJKPiumWr5OuFu80c",
+    authDomain: "rps-multiplayer-eb98c.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-eb98c.firebaseio.com",
+    projectId: "rps-multiplayer-eb98c",
+    storageBucket: "",
+    messagingSenderId: "96095048698",
+    appId: "1:96095048698:web:3e050f256b6e7265378a43"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+/* END FIREBASE */
+
+
 $(document).ready(() => {
     $('#exampleModal').on('show.bs.modal', event => {
         var button = $(event.relatedTarget);
@@ -6,3 +23,4 @@ $(document).ready(() => {
         
     });
 });
+
