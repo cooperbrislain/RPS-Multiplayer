cc8aa218d3ea0f8e49fa2f0c4dde8764725fd158
Cooper Brislain <cbrislain@gmail.com>
2019-09-16 15:20:37 -0700

    lotsastuff

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,48 @@
+const Game = {
+    Players: [
+        /* 
+        {
+            uuid: xxxxxxx,
+            ready: true/false,
+            move: null
+        }
+        */
+    ]
+}
+
+/**
+ * Fast UUID generator, RFC4122 version 4 compliant.
+ * @author Jeff Ward (jcward.com).
+ * @license MIT license
+ * @link http://stackoverflow.com/questions/105034/how-to-create-a-guid-uuid-in-javascript/21963136#21963136
+ **/
+var UUID = (function() {
+    var self = {};
+    var lut = []; for (var i=0; i<256; i++) { lut[i] = (i<16?'0':'')+(i).toString(16); }
+    self.generate = function() {
+        var d0 = Math.random()*0xffffffff|0;
+        var d1 = Math.random()*0xffffffff|0;
+        var d2 = Math.random()*0xffffffff|0;
+        var d3 = Math.random()*0xffffffff|0;
+        return lut[d0&0xff]+lut[d0>>8&0xff]+lut[d0>>16&0xff]+lut[d0>>24&0xff]+'-'+
+            lut[d1&0xff]+lut[d1>>8&0xff]+'-'+lut[d1>>16&0x0f|0x40]+lut[d1>>24&0xff]+'-'+
+            lut[d2&0x3f|0x80]+lut[d2>>8&0xff]+'-'+lut[d2>>16&0xff]+lut[d2>>24&0xff]+
+            lut[d3&0xff]+lut[d3>>8&0xff]+lut[d3>>16&0xff]+lut[d3>>24&0xff];
+    }
+    return self;
+})();
+
+let playerID = localStorage.getItem('playerID');
+if (playerID.length >= 10) {
+    console.log(`using player ID: ${playerID}`);
+} else {
+    playerID = UUID.generate();
+    console.log(`assigning player ID: ${playerID}`);
+    localStorage.setItem('playerID', playerID);
+}
+let nextMove = null;
 /* FIREBASE */
-var firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyACSShSnvrgEte-P-TJKPiumWr5OuFu80c",
     authDomain: "rps-multiplayer-eb98c.firebaseapp.com",
     databaseURL: "https://rps-multiplayer-eb98c.firebaseio.com",
@@ -10,17 +53,26 @@ var firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+
+let database = firebase.database();
 
 /* END FIREBASE */
 
 
 $(document).ready(() => {
-    $('#exampleModal').on('show.bs.modal', event => {
-        var button = $(event.relatedTarget);
-        var modal = $(this);
-        // Use above variables to manipulate the DOM
-        
+    $('.btn-rock').on('click', (e) => {
+        do_move('rock');
+    });
+
+    $('.btn-paper').on('click', (e) => {
+        do_move('paper');
+    });
+
+    $('.btn-scissors').on('click', (e) => {
+        do_move('scissors');
     });
 });
 
+database.ref().on("value", (snapshot) => {
+    Game = JSON.decode(snapshot);
+});
